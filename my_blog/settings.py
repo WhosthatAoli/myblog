86d34d20466ca78632bc9c8f98ab15932a6e954d
unknown <939677612@qq.com>
2020-04-28 10:49:37 +0800

    5

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -275,3 +275,5 @@ LOGIN_REDIRECT_URL = '/'
 #         },
 #     }
 # }
+
+django_heroku.settings(locals())
